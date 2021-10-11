3ddb963ec620e58bc31ae5747fba0bb75fde9a6f
Halit Celik <halit@cruncher.ch>
2021-10-11 23:06:40 +0200

    first functional test

--- functional_tests.py
+++ functional_tests.py
@@ -1,10 +1,22 @@
 from selenium import webdriver
-
-browser = webdriver.Firefox()
-
-browser.get("http://localhost:8000")
-
-assert "Django" in browser.title
+import unittest
 
 
-print("Test finished without errors")
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+        return super().tearDown()
+
+    def test_if_someone_can_start_a_list_and_retrieve_it(self):
+        self.browser.get("http://localhost:8000")
+        self.assertIn("To-Do", self.browser.title)
+        self.fail("Finish the test")
+
+
+if __name__ == "__main__":
+    unittest.main()
